9688e69182d8837370c47fbb7a4b8040fff2ad1a
Kyryl313 <flash73627@gmail.com>
2025-09-03 21:23:36 +0300

    Лаба1

---
+++ 1.py
@@ -0,0 +1,28 @@
+#змінні
+age = 6
+print('age',type(age),'=', age)
+name= 'Myroslav'
+print('name',type(name),'=', name)
+bal = 4.0
+print('bal',type(bal),'=', bal)
+perevedeno = True
+print('perevedeno',type(perevedeno),'=', perevedeno)
+spysok_ocinok = [4,4,3,5]
+print('spysok_ocinok',type(spysok_ocinok),'=', spysok_ocinok)
+mnozhyna = { 6,4,'Hello'}
+print('mnozhyna',type(mnozhyna),'=', mnozhyna)
+kortezh =  (1,2,4,3)
+print('kortezh',type(kortezh),'=', kortezh)
+slovnyk = {1: 'Ocinka'}
+print('slovnyk',type(slovnyk),'=', slovnyk)
+#оператори
+a=10
+b=4
+print('a + b=',a+b) #додавання
+print('a - b=',a-b) #віднімання
+print('a * b=',a*b) #множення
+print('a / b=',a/b) #ділення
+print('a % b=',a%b) #остача
+print('a ** b=',a**b) #степінь
+print('a // b=',a//b) #ділення без остачі
+
