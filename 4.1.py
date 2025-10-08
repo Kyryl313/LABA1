496815b52bed9e4b80f4eceff8032f52c307de40
Kyryl313 <flash73627@gmail.com>
2025-10-08 14:56:43 +0300

    4.2.py

--- 4.1.py
+++ 4.1.py
@@ -2,13 +2,13 @@ def format_price(price): #Функція1 - формат ціни
     return f"{price:.2f} грн"
 
 def check_availability(store, *items): #Функція2 - чек наявності
-    return {item: store.get(item, [0, False]) for item in items} #get - Returns the value of the specified key
+    return {item: (item in store and store[item][1]) for item in items}
 
 def order(store): #Функція3 - замовлення
     order = input("Введіть товари через пробіл: ").split()
     check = check_availability(store, *order)
 
-    unavailable = [item for item, data in check.items() if not data[1]] #data[1] це II-елемент списку тобто наявність
+    unavailable = [item for item, available in check.items() if not available]
 
     if unavailable:
         print("Замовлення скасовано! Немає:", ", ".join(unavailable)) #join обєднує в рядок через ", "
@@ -16,30 +16,27 @@ def order(store): #Функція3 - замовлення
 
     total = 0
     print("Замовлення:")
-    for item, (price, available) in check.items():
+    for item in order:
+        price = store[item][0]
         print(f"{item}: {format_price(price)}")
         total += price
     print("Загалом:", format_price(total))
 
 def main():
-    store = {
-        "Стіл": [1500, True],
-        "Стілець": [500, False],
-        "Лампа": [400, False],
-        "Шафа": [3200, True],
-        "Диван": [5800, True]}
+    store = {"Стіл": [1499.99, True],
+            "Стілець": [500.50, False],
+            "Лампа": [400.50, False],
+            "Шафа": [3199.99, True],
+            "Диван": [5800.25, True]}
 
     while True:
         choice = input("Оберіть дію (1 - Купити, 2 - Переглянути ціни):")
-
         if choice == "1":
             order(store)
-
         elif choice == "2":
             for item, (price, available) in store.items():
                 status = "+" if available else "-"
                 print(f"{item}: {format_price(price)} ({status})")
-
         else:
             print("Непрописаний варіант!")
 
