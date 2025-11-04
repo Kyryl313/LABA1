4d1d11da0441ae6dc585484e2e5e7d4e0cf6b480
Kyryl313 <flash73627@gmail.com>
2025-11-04 18:24:50 +0200

    6.1

---
+++ LAB6/6.1.py
@@ -0,0 +1,11 @@
+from decorator import track_history
+@track_history
+def numbers(a, b):
+    return a + b
+if __name__ == '__main__': #точка входу
+    numbers(0, 1)
+    numbers(0,2 )
+    numbers(0, 3)
+    numbers(0, 4)
+    numbers(0, 5)
+    numbers(0, 6)
